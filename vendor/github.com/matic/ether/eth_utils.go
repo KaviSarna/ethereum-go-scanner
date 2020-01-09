3b375eee84a96e1cd41da2dbaae88d1015afb275
Kavi Sarna <developer.kavi@gmail.com>
2020-01-09 12:08:38 +0530

    eth_util changes

--- vendor/github.com/matic/ether/eth_utils.go
+++ vendor/github.com/matic/ether/eth_utils.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"math/big"
+	"strconv"
 
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
@@ -51,25 +52,27 @@ func GetTransactionsDetails(client *ethclient.Client, block types.Block) []util.
 	var transactionsList = []util.Transaction{}
 
 	for _, tx := range block.Transactions() {
-		fmt.Println(tx.Hash().Hex())        // 0x5d49fcaa394c97ec8a9c3e7bd9e8388d420fb050a52083ca52ff24b3b65bc9c2
-		fmt.Println(tx.Value().String())    // 10000000000000000
-		fmt.Println(tx.Gas())               // 105000
-		fmt.Println(tx.GasPrice().Uint64()) // 102000000000
-		fmt.Println(tx.Nonce())             // 110644
-		fmt.Println(tx.Data())              // []
-		fmt.Println(tx.To().Hex())          // 0x55fE59D8Ad77035154dDd0AD0388D09Dd4047A8e
+		// fmt.Println(tx.Hash().Hex())        // 0x5d49fcaa394c97ec8a9c3e7bd9e8388d420fb050a52083ca52ff24b3b65bc9c2
+		// fmt.Println(tx.Value().String())    // 10000000000000000
+		// fmt.Println(tx.Gas())               // 105000
+		// fmt.Println(tx.GasPrice().Uint64()) // 102000000000
+		// fmt.Println(tx.Nonce())             // 110644
+		// fmt.Println(tx.Data())              // []
+		fmt.Println("Transaction to - ", tx.To().Hex())          // 0x55fE59D8Ad77035154dDd0AD0388D09Dd4047A8e
 
 		msg, err := tx.AsMessage(types.NewEIP155Signer(chainID))
 		if err != nil {
-			fmt.Println(msg.From().Hex()) // 0x0fD081e3Bb178dc45c0cb23202069ddA57064258
+			log.Fatal(err)
 		}
+		// fmt.Println(msg.From().Hex()) // 0x0fD081e3Bb178dc45c0cb23202069ddA57064258
 
 		transaction := util.Transaction{}
 		transaction.From = msg.From().Hex()
 		transaction.To = tx.To().Hex()
-		transaction.BlockNumber = string(block.Number().Uint64())
+		transaction.BlockNumber = strconv.Itoa(int(block.Number().Int64()))
 		transaction.TransactionHash = tx.Hash().Hex()
-
+                
+		fmt.Println("Transaction - ", transaction)
 		transactionsList = append(transactionsList, transaction)
 	}
 
