590bebfb00b77660e3794082831f5efd4ccd4b6d
Kavi Sarna <developer.kavi@gmail.com>
2020-01-09 12:54:14 +0530

    added router

--- main.go
+++ main.go
@@ -1,19 +1,27 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/matic/postgres"
 	"github.com/matic/util"
 )
 
 func main() {
-	http.HandleFunc("/transactions/{AccountId}", postgres.GetTransactions)
-	http.ListenAndServe(":8000", nil)
 
 	ethClient, _ := util.GetEthClient()
 	pgClient, _ := util.GetPostgresClient()
 
 	postgres.CacheBlocks(pgClient, ethClient)
 	postgres.SubscribeAndSaveBlocks(pgClient, ethClient)
+	
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello from server")
+	})
+	router.HandleFunc("/transactions/{AccountId}", postgres.GetTransactions).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8010", router))
 }
